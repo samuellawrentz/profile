fccf9288ef1c7c3943ed56cf889ff62b99547a6b
samuellawrentz <samuellawrentz@gmail.com>
2022-07-10 12:35:16 +0530

    Updated the layout for the quotes generator

--- src/pages/projects/quotes-generator/index.tsx
+++ src/pages/projects/quotes-generator/index.tsx
@@ -12,16 +12,33 @@ function QuotesGenerator() {
         
     })
   return (
-    <div id="sam-container">
-    <div className="heading">
-      <h1>Image Quote Generator</h1>
-      <div className="sub-heading">Built with ❤️ by Samuellawrentz</div>
-    </div>
+    <div className="container quotes-page">
+<div className='intro quotes-intro'>
+<h1>Image Quote Generator</h1>
+<div className="sub-heading text-regularLight">Built with ❤️ by Samuellawrentz</div>
+<div>
+  <p>This is a simple image generator that helps you put your quotes and download them as an image.</p>
+  <h3>How to use it?</h3>
+  <p>
+    <ol>
+      <li>The card loads up with some random picture</li>
+      <li>Enter your text/quote</li>
+      <li>Change/remove the author name</li>
+      <li>Use the refresh button to change the image</li>
+      <li>Click the download button once you are done with the changes</li>
+    </ol>
+  </p>
+  <p className='text-regularLight blockquote'>
+    More features like custom image, filters are on the way! Stay tuned.
+  </p>
+</div>
+</div>
+<div id="sam-container">
     <div className="contents">
       <div className="quote-container">
         <div className="image-container" style={{filter: 'brightness(1)'}}></div>
         <div className='quote'>
-          <div className="text" contentEditable>Type your quote here and then press download</div>
+          <div className="text" contentEditable>Type here.</div>
           <div className="author" contentEditable>Samuel Lawrentz</div>
         </div>
       </div>
@@ -33,6 +50,7 @@ function QuotesGenerator() {
       </div>
     </div>
   </div>
+    </div>
   )
 }
 
