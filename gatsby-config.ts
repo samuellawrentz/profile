c550bf014f403f969a7c4740c5bd51a4e1e1750f
samuellawrentz <samuellawrentz@gmail.com>
2022-07-19 16:07:42 +0530

    Added RSS support

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -81,6 +81,66 @@ const config: GatsbyConfig = {
 			},
 		  },
 		},
+	  }, 
+	  {
+		resolve: `gatsby-plugin-feed`,
+		options: {
+		  query: `
+			{
+			  site {
+				siteMetadata {
+				  title
+				  description
+				  siteUrl
+				  site_url: siteUrl
+				}
+			  }
+			}
+		  `,
+		  feeds: [
+			{
+			  serialize: ({ query: { site, allMdx } }: any) => {
+				return allMdx.nodes.map((node: any) => {
+				  return Object.assign({}, node.frontmatter, {
+					description: node.excerpt,
+					date: node.frontmatter.date,
+					url: site.siteMetadata.siteUrl + node.frontmatter.path,
+					guid: site.siteMetadata.siteUrl + node.frontmatter.path,
+					custom_elements: [{ "content:encoded": node.html }],
+				  })
+				})
+			  },
+			  query: `
+			  {
+			  allMdx(
+				sort: {fields: [frontmatter___date], order: DESC}
+				filter: {frontmatter: {published: {eq: true}}}
+			  ) {
+				nodes {
+				  id
+				  excerpt(pruneLength: 100)
+				  html
+				  frontmatter {
+					title
+					date(formatString: "DD MMM, YYYY")
+					path
+				  }
+				}
+			  }
+			}
+			  `,
+			  output: "/rss.xml",
+			  title: "Samuel's Blogs RSS Feed",
+			  // optional configuration to insert feed reference in pages:
+			  // if `string` is used, it will be used to create RegExp and then test if pathname of
+			  // current page satisfied this regular expression;
+			  // if not provided or `undefined`, all pages will have feed reference inserted
+			  match: "^/blog/",
+			  // optional configuration to specify external rss feed, such as feedburner
+			//   link: "https://feeds.feedburner.com/gatsby/blog",
+			},
+		  ],
+		},
 	  }],
 }
 
