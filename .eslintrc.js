899dae522fa94735d7fa2e7bdbd4e97e607aed53
Samuel Lawrentz <samuel.lawerence@plivo.com>
2023-12-23 22:48:41 +0530

    Fix for local storage issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,44 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:mdx/recommended",
+    "prettier",
+  ],
+  settings: {
+    "mdx/code-blocks": true,
+    // optional, if you want to disable language mapper, set it to `false`
+    // if you want to override the default language mapper inside, you can provide your own
+    "mdx/language-mapper": {},
+    react: {
+      version: "detect",
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-        "plugin:mdx/recommended",
-        "prettier",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "max-len": ["warn", { code: 108 }],
+    semi: 0,
+    "comma-dangle": ["warn", "always-multiline"],
+    "prettier/prettier": [
+      "warn",
+      {
+        tabWidth: 2,
+      },
     ],
-    settings: {
-        "mdx/code-blocks": true,
-        // optional, if you want to disable language mapper, set it to `false`
-        // if you want to override the default language mapper inside, you can provide your own
-        "mdx/language-mapper": {},
-        react: {
-            version: "detect",
-        },
-    },
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: ["react", "@typescript-eslint", "prettier"],
-    rules: {
-        "max-len": ["warn", { code: 108 }],
-        semi: 0,
-        "comma-dangle": ["warn", "always-multiline"],
-        "prettier/prettier": [
-            "warn",
-            {
-                tabWidth: 4,
-            },
-        ],
-    },
+  },
 };
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -8,126 +8,121 @@ import CircularProgress from "../components/circular-progress";
 import { StaticImage } from "gatsby-plugin-image";
 
 interface LayoutProps {
-    children?: React.ReactNode;
-    path?: string;
+  children?: React.ReactNode;
+  path?: string;
 }
 const texts = [
-    "Can you stay a bit longer?",
-    "Little more please?",
-    "Yay, thank you for your time!",
+  "Can you stay a bit longer?",
+  "Little more please?",
+  "Yay, thank you for your time!",
 ];
 const TimerBlock = () => {
-    const [tick, setTick] = useState(0);
-    const percent = tick / 0.6;
-    const date = new Date(0);
-    date.setSeconds(tick);
-    const timeString = date.toISOString().substr(14, 5);
-    useEffect(() => {
-        const interval = setInterval(
-            () =>
-                setTick((p) => {
-                    if (p > 60) clearInterval(interval);
-                    return p + 1;
-                }),
-            1000,
-        );
-    }, []);
-    return (
-        <div
-            className={`timer ${percent < 100 ? "visible" : ""}`}
-            title="This helps me increase the session time of my site. Thank you!"
-        >
-            <Block display="flex" alignItems="center" gap={16}>
-                <CircularProgress percent={percent} />
-                <div>{timeString}</div>
-            </Block>
-            <div className="text-caption">
-                {texts[Math.floor(percent / 39)]}
-            </div>
-        </div>
+  const [tick, setTick] = useState(0);
+  const percent = tick / 0.6;
+  const date = new Date(0);
+  date.setSeconds(tick);
+  const timeString = date.toISOString().substr(14, 5);
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setTick((p) => {
+          if (p > 60) clearInterval(interval);
+          return p + 1;
+        }),
+      1000,
     );
+  }, []);
+  return (
+    <div
+      className={`timer ${percent < 100 ? "visible" : ""}`}
+      title="This helps me increase the session time of my site. Thank you!"
+    >
+      <Block display="flex" alignItems="center" gap={16}>
+        <CircularProgress percent={percent} />
+        <div>{timeString}</div>
+      </Block>
+      <div className="text-caption">{texts[Math.floor(percent / 39)]}</div>
+    </div>
+  );
 };
 
 function Layout({ children, ...data }: LayoutProps) {
-    const is404 = data.path === "/404/";
-    const colorMode = localStorage.getItem("color-mode");
-    const [isDark, setIsDark] = useState(colorMode === "dark");
+  const is404 = data.path === "/404/";
+  const colorMode =
+    typeof window !== "undefined" && window.localStorage.getItem("color-mode");
+  const [isDark, setIsDark] = useState(colorMode === "dark");
 
-    return (
-        <div>
-            <Helmet
-                htmlAttributes={{
-                    lang: "en",
-                    "color-mode": isDark ? "dark" : "light",
-                }}
-            >
-                <meta
-                    name="ahrefs-site-verification"
-                    content="6c3839f0b764fa4c45002167b36cb3f3236bcedbbe54ab9c262661b28995374a"
-                ></meta>
-                <title>
-                    Samuel Lawrentz - The Frontend Dev - Blog, portfolio, web
-                    development
-                </title>
-            </Helmet>
-            <Block
-                className="logo"
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                gap={24}
-            >
-                <Link
-                    className="gradient"
-                    to="/"
-                    style={{ textDecoration: "none" }}
-                >
-                    <Block display="inline-flex" gap={16}>
-                        <StaticImage
-                            src="../assets/logo.jpg"
-                            alt="Samuel Lawrentz Logo"
-                            width={60}
-                        />
-                        <div>
-                            <div>Samuel Lawrentz</div>
-                            <div className="text-caption">The Frontend dev</div>
-                        </div>
-                    </Block>
-                </Link>
-                <span
-                    style={{ marginLeft: "auto" }}
-                    onClick={() => {
-                        // toggle the dark mode
-                        setIsDark(!isDark);
-                        // set the mode on the html element
-                        document.documentElement.setAttribute(
-                            "color-mode",
-                            isDark ? "dark" : "light",
-                        );
-                        localStorage.setItem(
-                            "color-mode",
-                            isDark ? "dark" : "light",
-                        );
-                    }}
-                >
-                    <Icon
-                        name={`${isDark ? "light" : "dark"}_mode`}
-                        size={32}
-                        title={`${isDark ? "Light" : "Dark"} mode`}
-                    />
-                </span>
-                <Link className="rss" to="/rss.xml">
-                    <Icon name="rss_feed" size={32} title="RSS feed" />
-                </Link>
-            </Block>
-            {/* <canvas id="canvas" className="background-canvas"></canvas>./ */}
-            <main>{children}</main>
-            <div className="time">
-                <TimerBlock />
+  return (
+    <div>
+      <Helmet
+        htmlAttributes={{
+          lang: "en",
+          "color-mode": isDark ? "dark" : "light",
+        }}
+      >
+        <meta
+          name="ahrefs-site-verification"
+          content="6c3839f0b764fa4c45002167b36cb3f3236bcedbbe54ab9c262661b28995374a"
+        ></meta>
+        <title>
+          Samuel Lawrentz - The Frontend Dev - Blog, portfolio, web development
+        </title>
+      </Helmet>
+      <Block
+        className="logo"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        gap={24}
+      >
+        <Link className="gradient" to="/" style={{ textDecoration: "none" }}>
+          <Block display="inline-flex" gap={16}>
+            <StaticImage
+              src="../assets/logo.jpg"
+              alt="Samuel Lawrentz Logo"
+              width={60}
+            />
+            <div>
+              <div>Samuel Lawrentz</div>
+              <div className="text-caption">The Frontend dev</div>
             </div>
-            {!is404 && <Footer />}
-        </div>
-    );
+          </Block>
+        </Link>
+        <span
+          style={{ marginLeft: "auto" }}
+          onClick={() => {
+            // toggle the dark mode
+            setIsDark(!isDark);
+            // set the mode on the html element
+            document.documentElement.setAttribute(
+              "color-mode",
+              isDark ? "dark" : "light",
+            );
+            typeof window !== "undefined" &&
+              window.localStorage.setItem(
+                "color-mode",
+                isDark ? "dark" : "light",
+              );
+          }}
+        >
+          <Icon
+            name={`${isDark ? "light" : "dark"}_mode`}
+            size={32}
+            title={`${isDark ? "Light" : "Dark"} mode`}
+          />
+        </span>
+        <Link className="rss" to="/rss.xml">
+          <Icon name="rss_feed" size={32} title="RSS feed" />
+        </Link>
+      </Block>
+      {/* <canvas id="canvas" className="background-canvas"></canvas>./ */}
+      <main>{children}</main>
+      <div className="time">
+        <TimerBlock />
+      </div>
+      {!is404 && <Footer />}
+    </div>
+  );
 }
 
 export default Layout;
