961ae2dfb39794b871f1fb1a404c435768dfd125
Samuel Lawrentz <samuel.lawerence@plivo.com>
2023-12-24 12:40:43 +0530

    added hygen

---
+++ _templates/blogpost/new/prompt.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    type: "input",
+    name: "title",
+    message: "What's the title of the blog post?",
+  },
+  {
+    type: "input",
+    name: "path",
+    message: "What's the path for this post? (e.g., my-first-post)",
+  },
+  {
+    type: "input",
+    name: "desc",
+    message: "What's the description for the post?",
+  },
+  {
+    type: "input",
+    name: "heroimage",
+    message: "Enter the path for the hero image.",
+  },
+  {
+    type: "input",
+    name: "tags",
+    message: "Enter tags for the post (comma-separated).",
+  },
+  {
+    type: "input",
+    name: "date",
+    message: "What's the date of the post? (format: YYYY-MM-DD)",
+    default: new Date().toISOString().split("T")[0], // Default to today's date
+  },
+];
