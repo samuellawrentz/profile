573daabb718e295ce7ed212eb7aafa5f7fdc310a
Samuel Lawrentz <samuel.lawerence@plivo.com>
2025-11-25 21:30:46 +0530

    Fixed member detail page

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -1,85 +1,85 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Helmet } from "react-helmet";
 import { useLocation } from "@reach/router";
-import { useStaticQuery, graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+import { Helmet } from "react-helmet";
 
 interface SEOProps {
-  title?: string;
-  description?: string;
-  image?: string;
-  article?: boolean;
+	title?: string;
+	description?: string;
+	image?: string;
+	article?: boolean;
 }
 
 const SEO = ({ title, description, image, article }: any) => {
-  const { pathname } = useLocation();
-  const { site } = useStaticQuery(query);
+	const { pathname } = useLocation();
+	const { site } = useStaticQuery(query);
 
-  const {
-    defaultTitle,
-    defaultDescription,
-    siteUrl,
-    defaultImage,
-    twitterUsername,
-  } = site.siteMetadata;
+	const {
+		defaultTitle,
+		defaultDescription,
+		siteUrl,
+		defaultImage,
+		twitterUsername,
+	} = site.siteMetadata;
 
-  const seo = {
-    title: title || defaultTitle,
-    description: description || defaultDescription,
-    image: `${siteUrl}${image || defaultImage}`,
-    url: `${siteUrl}${pathname}`,
-  };
+	const seo = {
+		title: title || defaultTitle,
+		description: description || defaultDescription,
+		image: `${siteUrl}${image || defaultImage}`,
+		url: `${siteUrl}${pathname}`,
+	};
 
-  return (
-    <Helmet title={seo.title}>
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+	return (
+		<Helmet title={seo.title}>
+			<meta name="description" content={seo.description} />
+			<meta name="image" content={seo.image} />
 
-            <meta name="google-adsense-account" content="ca-pub-7276855123882739">
+			<meta name="google-adsense-account" content="ca-pub-7276855123882739" />
 
-      {seo.url && <meta property="og:url" content={seo.url} />}
+			{seo.url && <meta property="og:url" content={seo.url} />}
 
-      <meta property="og:type" content={article ? "article" : "website"} />
+			<meta property="og:type" content={article ? "article" : "website"} />
 
-      {seo.title && <meta property="og:title" content={seo.title} />}
+			{seo.title && <meta property="og:title" content={seo.title} />}
 
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
+			{seo.description && (
+				<meta property="og:description" content={seo.description} />
+			)}
 
-      {seo.image && <meta property="og:image" content={seo.image} />}
+			{seo.image && <meta property="og:image" content={seo.image} />}
 
-      <meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:card" content="summary_large_image" />
 
-      {twitterUsername && (
-        <meta name="twitter:creator" content={twitterUsername} />
-      )}
+			{twitterUsername && (
+				<meta name="twitter:creator" content={twitterUsername} />
+			)}
 
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
+			{seo.title && <meta name="twitter:title" content={seo.title} />}
 
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
+			{seo.description && (
+				<meta name="twitter:description" content={seo.description} />
+			)}
 
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
-    </Helmet>
-  );
+			{seo.image && <meta name="twitter:image" content={seo.image} />}
+		</Helmet>
+	);
 };
 
 export default SEO;
 
 SEO.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  image: PropTypes.string,
-  article: PropTypes.bool,
+	title: PropTypes.string,
+	description: PropTypes.string,
+	image: PropTypes.string,
+	article: PropTypes.bool,
 };
 
 SEO.defaultProps = {
-  title: null,
-  description: null,
-  image: null,
-  article: false,
+	title: null,
+	description: null,
+	image: null,
+	article: false,
 };
 
 const query = graphql`
@@ -95,4 +95,3 @@ const query = graphql`
     }
   }
 `;
-
